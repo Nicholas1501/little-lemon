7d69e74e2da8d817621f2a4908107812aff3a15c
Nicholas1501 <nicholasfarrelxp@gmail.com>
2023-03-13 19:21:10 -0300

    added the menu API

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This project is a restaurant Django application with login, menu and table booking API's.
+Usage: Clone the repository and start the django server. After opening the application on your web browser, you can test the menu API by clicking on Menu.
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
+    path('menu/', views.MenuItemsView.as_view(), name='menu'),
+    path('menu/items/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
